191e8e07c285de35eacb8d15ac79d886d9431ed1
Siphesihle Madi <132097129+quadrills@users.noreply.github.com>
2025-08-18 16:10:44 +0200

    add createUsersTable method, establish connection to database, resolve slf4j warnings

--- src/main/java/org/example/DatabaseManager.java
+++ src/main/java/org/example/DatabaseManager.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseManager {
+    public static void createUsersDatabase(){
+        String url = "jdbc:sqlite:users.db";
+
+        String createUsersTable = "CREATE TABLE IF NOT EXISTS users ("+
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                "first_name TEXT NOT NULL, "+
+                "last_name TEXT NOT NULL, "+
+                "email TEXT NOT NULL, "+
+                "password TEXT NOT NULL)";
+
+        try(Connection conn = DriverManager.getConnection(url);
+        Statement stmt = conn.createStatement()){
+            stmt.execute(createUsersTable);
+            System.out.println("Table created or already exists.");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
