6582dd2a9d40c84f72c2c8a45154ca7449bf6932
Siphesihle Madi <132097129+quadrills@users.noreply.github.com>
2025-08-18 14:43:02 +0200

    create method to hash password

--- src/main/java/org/example/AuthManager.java
+++ src/main/java/org/example/AuthManager.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+
 public class AuthManager {
+    /**
+     * Method to hash password
+     */
+    public static String hashPassword(String password) {
+        Argon2 argon2 = Argon2Factory.create(
+                Argon2Factory.Argon2Types.ARGON2id
+        );
+        int iterations = 3;
+        int memory = 65536;
+        int parallelism = 1;
+
+        try{
+            return argon2.hash(iterations,memory,parallelism,password);
+        }finally {
+            argon2.wipeArray(password.toCharArray());
+        }
+    }
 }
