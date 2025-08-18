c6b15377e19c1d90f3516bc396424e89a57ed425
Siphesihle Madi <132097129+quadrills@users.noreply.github.com>
2025-08-18 15:03:15 +0200

    AuthManager.java code cleanup and refactoring, use updated hash and verify argon2 methods

--- src/main/java/org/example/AuthManager.java
+++ src/main/java/org/example/AuthManager.java
@@ -6,31 +6,35 @@ import de.mkammerer.argon2.Argon2Factory;
 public class AuthManager {
     /**
      * Method to hash password
+     * Argon2 automatically salts the password before the hashing process
+     * wipes the password after hashing
      */
     public static String hashPassword(String password) {
-        Argon2 argon2 = Argon2Factory.create(
-                Argon2Factory.Argon2Types.ARGON2id
-        );
+        char[] charArrayPassword = password.toCharArray();
+        Argon2 argon2 = Argon2Factory.create();
         int iterations = 3;
         int memory = 65536;
         int parallelism = 1;
 
-        try{
-            return argon2.hash(iterations,memory,parallelism,password);
-        }finally {
+        try {
+            return argon2.hash(iterations, memory, parallelism, charArrayPassword);
+        } finally {
             argon2.wipeArray(password.toCharArray());
         }
     }
+
     /**
      * method to verify a password against a stored hash
+     * wipes password after use
      */
-    public static boolean verifyPassword(String password, String storedHash){
+    public static boolean verifyPassword(String password, String storedHash) {
+        char[] charArrayPassword = password.toCharArray();
         Argon2 argon2 = Argon2Factory.create(
                 Argon2Factory.Argon2Types.ARGON2id
         );
-        try{
-            return argon2.verify(storedHash,password);
-        }finally {
+        try {
+            return argon2.verify(storedHash, charArrayPassword);
+        } finally {
             argon2.wipeArray(password.toCharArray());
         }
     }
